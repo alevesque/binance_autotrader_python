a5d0c80584b4a6ae5c25e6b77848e185b5ca545e
Alejandro Levesque <levesque.alejandro@gmail.com>
2021-03-26 21:07:47 -0400

    Add files via upload
    added error handling, refined implementation of coin-specific sig fig requirements, improved order function with error handling and varargs, check if initial orders were filled before submitting follow up orders, added more debugging outputs

--- binance_autotrader.py
+++ binance_autotrader.py
@@ -1,22 +1,20 @@
 from binance.client import Client
+from binance.exceptions import *
 #from binance.enums import *
 import time
+import sys
+import requests
 
 api_key = "3dUerAA0KFYj9BPhLajkRdx3yxcxgSWqyayemilGsFft4lmyXc9iqyATO5D5xVDX"
 api_secret = "8CgimgMn9JFajxCxfO49bAz1Sx8yGSxUuoCyg3Mix1i7SQlzXzuvBiWnVFBcBXAC"
 
-"""tld='' is used to denote which server to use (.com or .us)"""
-client = Client(api_key, api_secret,tld='us')
-
-def place_order(pair, side, order_type, time_in_force,qty,price):
-	order = client.create_order(
-	    symbol=pair,
-	    side=side,
-	    type=order_type,
-	    timeInForce=time_in_force,
-	    quantity=qty,
-	    price=price)
-	return order
+try:
+	#tld='' is used to denote which server to use (.com or .us)
+	client = Client(api_key, api_secret,{"timeout": 6},tld='us')
+	print("Client Initialized Successfully")
+except requests.exceptions.RequestException as e:
+	print("Connection error - Initializing API client")
+	sys.exit()
 
 def main():
 
@@ -24,17 +22,31 @@ def main():
 	fee = 0.001
 
 	"""factor of safety for trades ensures only trades with x% profit get executed, to help prevent loss"""
-	factor_of_safety=1.0075
-	spend_limit_for_testing = 0.02
-
+	factor_of_safety=1.005 #1.0075
+	spend_limit_for_testing = 0.01
+	buy_price_increase = 1.0005 # place initial order slighly above calculated price to help ensure order fills
 	trading_pairs = [("ADAUSD","ADABTC","ADA"),("BCHUSD","BCHBTC","BCH"),("BNBUSD","BNBBTC","BNB"),("ETHUSD","ETHBTC","ETH"),("LINKUSD","LINKBTC","LINK"),("LTCUSD","LTCBTC","LTC"),("UNIUSD","UNIBTC","UNI"),("VETUSD","VETBTC","VET"),("XTZUSD","XTZBTC","XTZ")]
+	
 	i=0
+
 	while 1:
 		i+=1
 		for x in trading_pairs:
-			USD_per_alt = client.get_avg_price(symbol=x[0])
-			BTC_per_alt = client.get_avg_price(symbol=x[1])
-			USD_per_BTC = client.get_avg_price(symbol='BTCBUSD')
+
+			try:
+				USD_per_alt = client.get_avg_price(symbol=x[0])
+				BTC_per_alt = client.get_avg_price(symbol=x[1])
+				USD_per_BTC = client.get_avg_price(symbol='BTCUSD')
+			except BinanceAPIException as e:
+				print(e)
+				print("Querying prices - something went wrong, likely connection issues. Trying again in 5s.")
+				time.sleep(5)
+				break
+			except requests.exceptions.RequestException as e:
+				print(e)
+				print("Error - Check connection - Querying prices")
+				time.sleep(5)
+				break
 
 			if i % 5 == 0:
 				print("Price: {} USD per {}".format(USD_per_alt["price"],x[2]))
@@ -54,68 +66,126 @@ def main():
 			"""
 			
 			if (float(USD_per_alt["price"])*(1+fee)*factor_of_safety < float(BTC_per_alt["price"])*(1-fee)*float(USD_per_BTC["price"])*(1-fee)):
+				
 				print("\a {} - EQ1 TRUE\n".format(x[2]))
 				verif_str1 = "[1] USD per {}: {}\n[2] BTC per {}: {}\n[3] USD per BTC: {}\n\n[1] USD + 0.001*[1] (0.1% fee) buys 1 {} -> 1 {} - 0.001 {} (0.1% fee) buys 0.999*[2] BTC -> 0.999*[2] BTC - 0.001*0.999*[2] BTC (0.1% fee) sells for (0.999^2)*[3] USD.\n"
 				print(verif_str1.format(x[2],USD_per_alt["price"],x[2],BTC_per_alt["price"],USD_per_BTC["price"],x[2],x[2],x[2]))
 				
-				usd_balance = client.get_asset_balance(asset='USD')
-				print("USD balance before trade: {}".format(usd_balance["free"]))
-
-				if (x[2]=="VET"):
-					USD_per_alt_order = place_order(x[0],'BUY','LIMIT','IOC',"{:.0f}".format(spend_limit_for_testing*float(usd_balance["free"])/float(USD_per_alt["price"])),"{:.4f}".format(1.001*float(USD_per_alt["price"])))
-				elif (x[2]=="ADA"):
-					USD_per_alt_order = place_order(x[0],'BUY','LIMIT','IOC',"{:.1f}".format(spend_limit_for_testing*float(usd_balance["free"])/float(USD_per_alt["price"])),"{:.4f}".format(1.001*float(USD_per_alt["price"])))
+				usd_balance = check_balance('USD')
+				if (usd_balance == -1):
+					break
+							
+				USD_per_alt_order = place_order(symbol=x[0],side='BUY',type='LIMIT',timeInForce='IOC',quantity="{0:.{1}f}".format(spend_limit_for_testing*float(usd_balance)/float(USD_per_alt["price"]),alt_precision(x[2])),price="{:.4f}".format(buy_price_increase*float(USD_per_alt["price"])))
+				
+				if (USD_per_alt_order == -1):
+					break
 				else:
-					USD_per_alt_order = place_order(x[0],'BUY','LIMIT','IOC',"{:.4f}".format(spend_limit_for_testing*float(usd_balance["free"])/float(USD_per_alt["price"])),"{:.4f}".format(1.001*float(USD_per_alt["price"])))
+					print('Order for {:.2f} {} at {:.4f} USD ea completed with status: {}.\n'.format(float(USD_per_alt_order["executedQty"]),x[2],float(USD_per_alt_order["price"]),USD_per_alt_order["status"]))
 				
-				print('Order for {:.2f} {} at {:.4f} USD ea completed with status: {}.\n'.format(float(USD_per_alt_order["executedQty"]),x[2],float(USD_per_alt_order["price"]),USD_per_alt_order["status"]))
-				
-				#time.sleep(0.1)
-				
-				BTC_per_alt_order = place_order(x[1],'SELL','MARKET',"{:.4f}".format(float(USD_per_alt_order["executedQty"])))#,"{:.4f}".format(float(BTC_per_alt["price"])))
-				print('Order for {:.2f} BTC at {:.4f} {} ea completed with status: {}.\n'.format(float(BTC_per_alt_order["executedQty"]),x[2],(1/float(BTC_per_alt_order["price"])),BTC_per_alt_order["status"]))
-				
-				#time.sleep(0.1)
-				
-				USD_per_BTC_order = place_order('BTCBUSD','SELL','MARKET',"{:.4f}".format(float(BTC_per_alt_order["executedQty"])))#,"{:.4f}".format(float(USD_per_BTC["price"])))
-				print('Order for {:.2f} USD at {:.4f} BTC ea completed with status: {}.\n'.format(float(USD_per_BTC_order["executedQty"]),(1/float(USD_per_BTC_order["price"])),USD_per_BTC_order["status"]))
-				
-				usd_balance = client.get_asset_balance(asset='USD')
-				print("USD balance after trade: {}".format(usd_balance["free"]))
-				break
+				if(USD_per_alt_order["status"]=='FILLED'):
+					BTC_per_alt_order = place_order(symbol=x[1],side='SELL',type='MARKET',quantity="{0:.{1}f}".format(float(USD_per_alt_order["executedQty"]),alt_precision(x[2])))#,"{:.4f}".format(float(BTC_per_alt["price"])))
+									
+					if (BTC_per_alt_order == -1):
+						break
+					else:
+						print(BTC_per_alt_order)
+						print('Order for {:.2f} BTC at {} {} ea completed with status: {}.\n'.format(float(BTC_per_alt_order["executedQty"]),(1/float(BTC_per_alt_order["fills"][0]["price"])),x[2],BTC_per_alt_order["status"]))
 
+				
+					USD_per_BTC_order = place_order(symbol='BTCUSD',side='SELL',type='MARKET',quantity="{:.6f}".format(float(BTC_per_alt_order["executedQty"])))#,"{:.4f}".format(float(USD_per_BTC["price"])))
+					
+					if (USD_per_BTC_order == -1):
+						break
+					else:
+						print('Order for {:.2f} USD at {:.6f} BTC ea completed with status: {}.\n'.format(float(USD_per_BTC_order["executedQty"]),(1/float(USD_per_BTC_order["fills"][0]["price"])),USD_per_BTC_order["status"]))
+		
+					usd_balance = check_balance('USD')
+				
+					time.sleep(10000)
 			elif (float(USD_per_BTC["price"])*(1+fee)*factor_of_safety < float(USD_per_alt["price"])*(1-fee)/(float(BTC_per_alt["price"])*(1-fee))):
+				
 				print("\a {} - EQ2 TRUE\n".format(x[2]))
 				verif_str2 = "[1] USD per BTC: {}\n[2] {} per BTC: {}\n[3] USD per {}: {}\n\n[1] USD + 0.001*[1] (0.1% fee) buys 1 BTC -> 1 BTC - 0.001 BTC (0.1% fee) buys 0.999*[2] {} -> 0.999*[2] {} - 0.001*0.999*[2] {} (0.1% fee) sells for (0.999^2)*[3] USD.\n"
 				print(verif_str2.format(USD_per_BTC["price"],x[2],str(1/float(BTC_per_alt["price"])),x[2],USD_per_alt["price"],x[2],x[2],x[2]))
 				
-				usd_balance = client.get_asset_balance(asset='USD')
-				print("USD balance before trade: {}".format(usd_balance["free"]))
+				usd_balance = check_balance('USD')
+				if (usd_balance == -1):
+					break
 
-				USD_per_BTC_order = place_order('BTCBUSD','BUY','LIMIT','IOC',"{:.4f}".format(spend_limit_for_testing*float(usd_balance["free"])/float(USD_per_BTC["price"])),"{:.2f}".format(1.001*float(USD_per_BTC["price"])))
-				print('Order for {:.2f} BTC at {:.4f} USD ea completed with status: {}.\n'.format(float(USD_per_BTC_order["executedQty"]),float(USD_per_BTC_order["price"]),USD_per_BTC_order["status"]))
-				#
-				time.sleep(0.1)
+				btc_quantity = round((spend_limit_for_testing*float(usd_balance)/float(USD_per_BTC["price"]))/BTC_per_alt["price"],alt_precision(x[2]))*BTC_per_alt["price"]
+				print(btc_quantity)
 
-				if (x[2]=="VET"):
-					BTC_per_alt_order = place_order(x[1],'SELL','MARKET',"{:.0f}".format(float(USD_per_BTC_order["executedQty"])))#,"{:.4f}".format(float(BTC_per_alt["price"])))
-				elif (x[2]=="ADA"):
-					BTC_per_alt_order = place_order(x[1],'SELL','MARKET',"{:.1f}".format(float(USD_per_BTC_order["executedQty"])))#,"{:.4f}".format(float(BTC_per_alt["price"])))
+				USD_per_BTC_order = place_order(symbol='BTCUSD',side='BUY',type='LIMIT',timeInForce='IOC',quantity="{:.6f}".format(btc_quantity),price="{:.2f}".format(buy_price_increase*float(USD_per_BTC["price"])))
+				if (USD_per_BTC_order == -1):
+					break
 				else:
-					BTC_per_alt_order = place_order(x[1],'SELL','MARKET',"{:.4f}".format(float(USD_per_BTC_order["executedQty"])))#,"{:.4f}".format(float(BTC_per_alt["price"])))
+					print('Order for {} BTC at {} USD ea completed with status: {}.\n'.format(float(USD_per_BTC_order["executedQty"]),float(USD_per_BTC_order["price"]),USD_per_BTC_order["status"]))
 				
-				print('Order for {:.2f} {} at {:.4f} {} ea completed with status: {}.\n'.format(float(BTC_per_alt_order["executedQty"]),x[2],(1/float(BTC_per_alt_order["price"])),x[2],BTC_per_alt_order["status"]))
+				if(USD_per_BTC_order["status"]=='FILLED'):
+
+					BTC_per_alt_order = place_order(symbol=x[1],side='BUY',type='MARKET',quantity="{0:.{1}f}".format(float(USD_per_BTC_order["executedQty"])/float(BTC_per_alt["price"]),alt_precision(x[2])))#,"{:.4f}".format(float(BTC_per_alt["price"])))
+					
+					if (BTC_per_alt_order == -1):
+						break
+					else:
+						print('Order for {} {} at {:.6f} BTC ea completed with status: {}.\n'.format(float(BTC_per_alt_order["executedQty"]),x[2],float(BTC_per_alt_order["fills"][0]["price"]),BTC_per_alt_order["status"]))
+					
+					USD_per_alt_order = place_order(symbol=x[0],side='SELL',type='MARKET',quantity="{0:.{1}f}".format(float(BTC_per_alt_order["executedQty"]),alt_precision(x[2])))#,"{:.4f}".format(float(USD_per_alt["price"])))
+					
+					if (USD_per_alt_order == -1):
+						break
+					else:
+						print('Order for {:.2f} USD at {:.4f} {} ea completed with status: {}.\n'.format(float(USD_per_BTC_order["executedQty"]),(1/float(USD_per_BTC_order["fills"][0]["price"])),x[2],USD_per_BTC_order["status"]))
 				
-				#time.sleep(0.1)
-				
-				USD_per_alt_order = place_order(x[0],'SELL','MARKET',"{:.4f}".format(float(BTC_per_alt_order["executedQty"])))#,"{:.4f}".format(float(USD_per_alt["price"])))
-				print('Order for {:.2f} USD at {:.4f} {} ea completed with status: {}.\n'.format(float(USD_per_BTC_order["executedQty"]),(1/float(USD_per_BTC_order["price"])),x[2],USD_per_BTC_order["status"]))
-			
-				usd_balance = client.get_asset_balance(asset='USD')
-				print("USD balance after trade: {}".format(usd_balance["free"]))
-				break
-				
+					usd_balance = check_balance('USD')	
+					time.sleep(10000)			
 			
 		time.sleep(1)
-		
+
+def place_order(**order_parameters): #symbol, side, type, timeInForce,quantity,price):
+	print(order_parameters)
+
+	try:
+		order = client.create_order(**order_parameters)
+		return order
+	except BinanceAPIException as e:
+		print(e)
+		print("placing order - something went wrong.")
+		time.sleep(1)
+		return -1
+	except requests.exceptions.RequestException as e:
+		print(e)
+		print("Error - Check connection - placing order")
+		time.sleep(1)
+		return -1
+
+def check_balance(requested_asset):
+	try:
+		asset_balance = client.get_asset_balance(asset=requested_asset)
+		print("Balance: {} {}".format(asset_balance["free"],requested_asset))
+		return asset_balance["free"]
+	except BinanceAPIException as e:
+		print(e)
+		print("Querying {} balance - something went wrong.".format(requested_asset))
+		time.sleep(1)
+		return -1
+	except requests.exceptions.RequestException as e:
+		print(e)
+		print("Error - Check connection - Querying {} balance.".format(requested_asset))
+		time.sleep(1)
+		return -1
+
+def alt_precision(x):
+	return {
+		'ADA': 0,
+		'BCH': 2,
+		'BNB': 2,
+		'ETH': 2,
+		'LINK': 1,
+		'LTC': 2,
+		'UNI': 0,
+		'VET': 0,
+		'XTZ': 2,
+	}
+
 main()
