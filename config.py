ef684e3db9e06bb97536c0186e2a07e3ef187442
Alejandro Levesque <levesque.alejandro@gmail.com>
2023-03-16 16:52:53 -0400

    create Order class, remove x_order_finished logic

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 15:11:08 2023
+
+@author: alevesque
+"""
+
 #fee can be reduced by extensive BNB holdings, high monthly trade volume (USD), or 25% discount for paying fees with BNB. see https://www.binance.us/en/fee/schedule
 fee = 0.001
 
@@ -6,8 +13,8 @@ reset_orders = True
 
 if use_testnet:
 	#KEYS FOR TESTNET
-	api_key = "SAnwsmicA0Z35tMi4y4oIdbgVBzX6N6bLzEvUwcYDMXGk3XnUdEwN4NAFmNDy1aw"
-	api_secret = "558hkhICPOKwrwsFEAvy0mDuGwALcWwTjeaTFhemUEapfmQJNo7TczXbNWbsH1JK"
+	API_KEY = "SAnwsmicA0Z35tMi4y4oIdbgVBzX6N6bLzEvUwcYDMXGk3XnUdEwN4NAFmNDy1aw"
+	API_SECRET = "558hkhICPOKwrwsFEAvy0mDuGwALcWwTjeaTFhemUEapfmQJNo7TczXbNWbsH1JK"
 
 	#WALLET ASSETS
 	balance = {
@@ -16,8 +23,8 @@ if use_testnet:
 				}
 else:
 	#KEYS FOR REAL SERVER
-	api_key = "3dUerAA0KFYj9BPhLajkRdx3yxcxgSWqyayemilGsFft4lmyXc9iqyATO5D5xVDX"
-	api_secret = "8CgimgMn9JFajxCxfO49bAz1Sx8yGSxUuoCyg3Mix1i7SQlzXzuvBiWnVFBcBXAC"
+	API_KEY = "3dUerAA0KFYj9BPhLajkRdx3yxcxgSWqyayemilGsFft4lmyXc9iqyATO5D5xVDX"
+	API_SECRET = "8CgimgMn9JFajxCxfO49bAz1Sx8yGSxUuoCyg3Mix1i7SQlzXzuvBiWnVFBcBXAC"
 
 	#WALLET ASSETS
 	balance = {
@@ -25,7 +32,7 @@ else:
 				'XLM': 0
 				}
 
-
+ALIVE_ORDER_STATUS = ['New', 'PARTIALLY_FILLED']
 #store persistent values and constants that carry over between calls of trading_strategy()
 carryover_vars = {
 					'prev_max_balance': 0,
@@ -37,8 +44,6 @@ carryover_vars = {
 					'sellprice': 0,
 					'selltime': 0,
 					'COMMRATE': 0.001,
-					'buy_order_finished': 1,
-					'sell_order_finished': 1,
 					'sell_cond_count': [0,0,0,0]
 }
 
@@ -51,3 +56,35 @@ trading_pairs = [
 
 					}
 ]
+
+class Order():
+    def __init__(self):
+        self.order = {}
+        self.reset()
+    
+    def set(self, **kwargs):
+        for kw in kwargs:
+            self.order[kw] = kwargs[kw]
+        
+    def reset(self):
+        self.order.clear()
+        self.set(
+            **{
+                'symbol': None,
+                'side': None, #buy/sell
+                'type': None, #limit, market, etc
+                'timeInForce': None,
+                'quantity': 0.0,
+                'price': 0,
+                'orderId': 0,
+                'time': 0,
+                'status': None,
+                'commission': 0
+                }
+            )
+    
+    def alive(self):
+        if self.order['status']:
+            return True
+        else:
+            return False
